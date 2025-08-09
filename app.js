f0f56abd76c491ee6cb522786eadd5c505ff9940
SarahBiscuit <scfletcher2000@hotmail.com>
2025-08-09 20:08:41 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+//Main entry point for the application
+
+import express from "express";
+import bodyParser from "body-parser";
+
+/* Lifted from my to do list website.
+
+Add a version of this for the book notes website, once I have written the items.js.
+
+import { addItem, getItems, deleteItem } from './api/items.js'; */
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+/* Render blank index.js page with default user */
+app.get('/', (req, res) => {
+  /* ADD THE RES.RENDER IN HERE */
+  });
+});
+
+app.post('/addBook', async (req, res) => {
+  /* Handles form submission for adding a book */
+});
+
+app.post ('/addUser', async (req, res) => {
+    /* Handles form submission for adding a user */
+})
+
+app.post('/addUserPage', async (req, res) => {
+    /* Renders the new user form page */
+})
+
+app.post('/addBookPage', async (req, res) => {
+    /* Renders the new book form page */
+})
+
+app.post('/edit', async (req, res) => {
+    /* Allows user to edit book details */
+})
+
+app.post('/delete', async (req, res) => {
+    /* Allows user to delete a book */
+})
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
